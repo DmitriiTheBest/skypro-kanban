d54102c4808b8f3c131df6d7c9df12615f0ed694
DmitriiThe <>
2024-02-06 01:50:47 +0300

    created data.js with an Array with cards - step 2

---
+++ vite-project/src/data.js
@@ -0,0 +1,24 @@
+// создание массива объектов с полями id, topic, title, date и status
+export const cardList = [
+  {
+    id: 1,
+    topic: "Web design",
+    title: "Task name",
+    date: "30.10.2023",
+    status: "Without a status",
+  },
+  {
+    id: 2,
+    topic: "Research",
+    title: "Task name",
+    date: "30.10.2023",
+    status: "Without a status",
+  },
+  {
+    id: 3,
+    topic: "Copywriting",
+    title: "Task name",
+    date: "30.10.2023",
+    status: "Without a status",
+  },
+];

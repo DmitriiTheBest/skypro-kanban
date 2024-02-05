7a0ac7249e081c0c4920bf439e97730bc291bcea
DmitriiThe <>
2024-02-06 01:36:02 +0300

    создание useState onClick и открытие модалки с именем - пункт 1

--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [isOpened, setIsOpened] = useState(false); // Состояние открытия модального окна
+  function togglePopUp() {
+    // Функция открытия модального окна
+    setIsOpened(!isOpened);
+  }
   return (
     <header class="header">
       <div class="container">
@@ -17,21 +24,30 @@ export default function Header() {
             <button class="header__btn-main-new _hover01" id="btnMainNew">
               <a href="#popNewCard">Создать новую задачу</a>
             </button>
-            <a href="#user-set-target" class="header__user _hover02">
+            {/* навешиваем обработчик события onClick для открытия модального окна */}
+            <div onClick={togglePopUp} class="header__user _hover02">
               Ivan Ivanov
-            </a>
-            <div class="header__pop-user-set pop-user-set" id="user-set-target">
-              {/* <!-- <a href="">x</a> --> */}
-              <p class="pop-user-set__name">Ivan Ivanov</p>
-              <p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-              <div class="pop-user-set__theme">
-                <p>Темная тема</p>
-                <input type="checkbox" class="checkbox" name="checkbox" />
-              </div>
-              <button type="button" class="_hover03">
-                <a href="#popExit">Выйти</a>
-              </button>
             </div>
+            {/* создать зависимость модального окна от переменной isOpened 
+            Если isOpened = true, то открывается модальное окно
+            если isOpened = false, то модальное окно закрывается */}
+            {isOpened && (
+              <div
+                class="header__pop-user-set pop-user-set"
+                id="user-set-target"
+              >
+                {/* <!-- <a href="">x</a> --> */}
+                <p class="pop-user-set__name">Ivan Ivanov</p>
+                <p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                <div class="pop-user-set__theme">
+                  <p>Темная тема</p>
+                  <input type="checkbox" class="checkbox" name="checkbox" />
+                </div>
+                <button type="button" class="_hover03">
+                  <a href="#popExit">Выйти</a>
+                </button>
+              </div>
+            )}
           </nav>
         </div>
       </div>

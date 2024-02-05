ca891734d33f01b6896b8ffbc81e874e0a32979f
DmitriiThe <>
2024-02-06 02:20:09 +0300

    decorated columns with cards - step 3

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,6 +7,15 @@ import PopBrowse from "./Components/POPUPs/PopBrowse/PopBrowse";
 import PopExit from "./Components/POPUPs/PopExit/PopExit";
 import PopNewCard from "./Components/POPUPs/PopNewCard/PopNewCard";
 
+// для отрисовки колонки с карточками создаем список статусов
+const statusList = [
+  "Without a status",
+  "Need to do",
+  "In working",
+  "Testing",
+  "Ready",
+];
+
 export default function App() {
   return (
     <>
@@ -21,11 +30,19 @@ export default function App() {
         {/* Сделали тег MainContent парным и передали в него Column из MainContent.jsx 
         Всё, что находится между открывающим и закрывающим тегом - это Children */}
         <MainContent>
-          <Column title={"Without a status"} />
+          {/* отрисовка списаков со статусами при помощи метода map так как это массив 
+          Передаем в него статусы из массива statusList 
+          Метод map принимает как аргумент callback функцию 
+          первый параметр функции - элемент status 
+          Элемент Status принимает в качестве пропсов title и key */}
+          {statusList.map((status) => {
+            return <Column title={status} key={status} />;
+          })}
+          {/* <Column title={"Without a status"} />
           <Column title={"Need to do"} />
           <Column title={"In working"} />
           <Column title={"Testing"} />
-          <Column title={"Ready"} />
+          <Column title={"Ready"} /> */}
         </MainContent>
       </div>
     </>

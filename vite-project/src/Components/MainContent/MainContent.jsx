b4679fda90f12c9396143e9d90153959c11e9110
DmitriiThe <>
2024-03-09 14:31:13 +0300

    Made and styled MainContent Component

--- vite-project/src/Components/MainContent/MainContent.jsx
+++ vite-project/src/Components/MainContent/MainContent.jsx
@@ -1,14 +1,17 @@
 // импортируем styled-component для Container
 import { Container } from "../../styled/common/common.styled";
+import { MainBlockStyled, MainContentStyled, MainStyled } from "./MainContent.Styled";
+
+// import * as S from "./MainContent.styled";
 
 export default function MainContent({ children }) {
   return (
-    <main className="main">
+    <MainStyled>
       
-      {/* заменить тег 'div' c классом 'Container' на styled-component 'Container'  */}
+    {/* заменяем тег 'div' c классом 'Container' на styled-component 'Container' из Common.styled.js  */}
       <Container>
-        <div className="main__block">
-          <div className="main__content">
+        <MainBlockStyled>
+          <MainContentStyled>
             {/* Скопировали содержимое "main__column column" в отдельный компонент
                 Здесь вызываем компонент через Props  */}
             {/* <Column title={"Without a status"} />
@@ -20,9 +23,9 @@ export default function MainContent({ children }) {
             {/* Заменяем все строки Column на prop children,  
                 Строки Column копируем и передаем в App.jsx в тег MainContent*/}
             {children}
-          </div>
-        </div>
+          </MainContentStyled>
+        </MainBlockStyled>
       </Container>
-    </main>
+    </MainStyled>
   );
 }

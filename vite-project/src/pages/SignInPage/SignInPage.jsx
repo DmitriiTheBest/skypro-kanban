4a84f85705061b9f1b1234a70a11763b4e8e4c29
DmitriiThe <>
2024-04-23 21:30:19 +0300

    создание состояния для INPUT и функции handleInputChange в SignIn и стили для SignInPage.jsx

--- vite-project/src/pages/SignInPage/SignInPage.jsx
+++ vite-project/src/pages/SignInPage/SignInPage.jsx
@@ -1,7 +1,23 @@
-// создать заглушки для страницы компонента SignInPage
-// import { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { appRoutes } from "../../lib/appRoutes";
+
+import "./SignIn.css";
 
 export default function SignInPage({ login }) {
+  const [loginData, setLoginData] = useState({
+    login: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target; // Извлекаем имя поля и его значение
+
+    setLoginData({
+      ...loginData, // Копируем текущие данные из состояния
+      [name]: value, // Обновляем нужное поле
+    });
+  };
+
   return (
     <div className="wrapper">
       <div className="container-signin">
@@ -12,6 +28,8 @@ export default function SignInPage({ login }) {
             </div>
             <form className="modal__form-login" id="formLogIn" action="#">
               <input
+                value={loginData.login}
+                onChange={handleInputChange}
                 className="modal__input"
                 type="text"
                 name="login"
@@ -19,13 +37,21 @@ export default function SignInPage({ login }) {
                 placeholder="Эл. почта"
               />
               <input
+                value={loginData.password}
+                onChange={handleInputChange}
                 className="modal__input"
                 type="password"
                 name="password"
                 id="formpassword"
                 placeholder="Пароль"
               />
-              <button onClick={login} className="modal__btn-enter _hover01" id="btnEnter">Войти</button>
+              <button
+                onClick={login}
+                className="modal__btn-enter _hover01"
+                id="btnEnter"
+              >
+                Войти
+              </button>
               <div className="modal__form-group">
                 <p>Нужно зарегистрироваться?</p>
                 <a href="signup.html">Регистрируйтесь здесь</a>
